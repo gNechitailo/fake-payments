6a469709ac5277e99eaab5dd6eca9ff4b85b6c07
Grigorii Nechitailo <g.nechitailo@dunice.net>
2019-08-26 12:40:42 +0300

    for the greater good

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,109 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 
-function randomHash(len = 32) {
-  let stringlen = 0;
-  let str = '';
-  while (stringlen < len) {
-    str += Math.random().toString(36).substr(2, 10);
-    stringlen += 10;
+currencies = {
+  RUB: 643,
+  KZT: 398,
+};
+
+const lalaMap = new Map();
+
+lalaMap.set('7lzf78m21d5v6m356wq4f5nwi35dscaf', { // the obrazets
+  response: {
+    result_code: 0,
+    "bill": {
+      "bill_id": '7lzf78m21d5v6m356wq4f5nwi35dscaf',
+      "amount": 100,
+      "ccy": "RUB",
+      "status": "waiting",
+      "error": 0,
+      "user": "tel:+71111111111",
+    },
+  },
+});
+
+router.get('/redirect-me', function (req, res) {
+  const whereToRedirect = req.query.successUrl;
+  console.log(whereToRedirect);
+  if (whereToRedirect) {
+    res.redirect(whereToRedirect);
+    return;
   }
-  return str.substr(0, len);
+  res.status(400).json({ error: dunno });
+});
+
+function sendPayment(toSend) {
+  axios.post('http://localhost:7846/payment', toSend).then((what) => {
+    console.log(what);
+  }, (err) => {
+    console.log(err);
+  });
 }
 
-/* GET home page. */
-router.put('/', function(req, res) {
-  const body = req;
+router.get('/fasta', function (req, res) {
+  const whatToSend = { // also obrazets
+    "amount": 100,
+    "currency": 643,
+    "walletPhone":71111111111,
+    "account": 79222222222,
+    "extra": {
+      "txn_id": "7lzf78m21d5v6m356wq4f5nwi35dscaf",
+      "source": "qw",
+      "alias": "easyaddress"
+    },
+  };
+  res.status(200).json({ what: 'Sending smart shit' });
+  setTimeout(() => {
+    sendPayment(whatToSend)
+  })
+});
+
+router.get('/invoice-me/:num/bills/:trhash', function(req, res) {
+  const { trhash } = req.params;
+  const data = lalaMap.get(trhash);
+  res.status(200).json(data);
+});
+
+/* PUT invoice http://api.qiwi.com/api/v2/prv/%d/bills/%s invoice-me/%d/bills/%s  */
+router.put('/invoice-me/:num/bills/:trhash', function(req, res) {
+  const { body } = req;
+  const { trhash } = req.params;
+  console.log('body =', body);
   console.log('req.params = ', req.params);
+  console.log(req.query);
+  // const hash = randomHash(32);
+  const responseData = {
+    response: {
+      "result_code": 0,
+      "bill": {
+        "bill_id": trhash,
+        "amount": body.amount,
+        "ccy": body.ccy,
+        "status": "waiting",
+        "error": 0,
+        "user": body.user,
+      },
+    },
+  };
+  lalaMap.set(trhash, responseData)
+  res.status(200).send(JSON.stringify(responseData));
   setTimeout(function () {
-      const str = {
-        "result_code": 0,
-        "bill": {
-          "bill_id": randomHash(32),
-          "amount": "100.00",
-          "ccy": "RUB",
-          "status": "waiting",
-          "error": 0,
-          "user": "tel:+71111111111"
-        }
-      };
-      res.send(JSON.stringify(str));
-  }, 2000);
+    const walletPhoneStr = body.user.substring(5)
+    const toSend = {
+      amount: parseFloat(body.amount),
+      currency: currencies[body.ccy],
+      account: parseInt(body.account, 10),
+      walletPhone: parseInt(walletPhoneStr, 10),
+      extra: {
+        txn_id: trhash,
+        source: body['extras[source]'],
+        alias: body['extras[alias]'],
+      },
+    };
+    console.log(JSON.stringify(toSend));
+    sendPayment(toSend);
+  }, 10000);
 });
 
 module.exports = router;
